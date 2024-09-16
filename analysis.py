be28f4e3677f187cec9129b950242bef4f775170
Pacidus <pacidus@gmail.com>
2024-09-16 12:51:29 +0200

    Clean of the analysis code

--- analysis.py
+++ analysis.py
@@ -3,6 +3,68 @@ import polars as pl
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
+###############
+## Functions ##
+###############
+
+def fig_histogram(Born, Death):
+    Yn, Nn = Born
+    Yd, Nd = Death
+
+    fig = plt.figure()
+    fig.set_size_inches(12, 4)
+    ax1 = fig.add_axes((0.07,0.12,0.42,0.86))
+    ax2 = fig.add_axes((0.51,0.12,0.42,0.86), sharey=ax1)
+    plt.setp(ax2.get_yticklabels(), visible=False)
+
+    ax1.bar(Yn, Nn, 1, color="#559")
+    ax1.legend(["Birth year of the desceased"])
+    ax1.grid(linestyle="--")
+    ax1.set_ylabel("Number")
+    ax1.set_xlabel("Year")
+
+    ax2.bar(Yd, Nd, 1, color="#644")
+    ax2.legend(["Year of death"])
+    ax2.grid(linestyle="--")
+    ax2.set_xlabel("Year")
+    return fig
+
+def fig_2Dhist(Born, Death, X, Y, count):
+    Yn, Nn = Born
+    Yd, Nd = Death
+    kwargs = {
+        "frameon" : False,
+        "xmargin" : 0,
+        "ymargin" : 0,
+        }
+
+    fig = plt.figure()
+    fig.set_size_inches(8, 8)
+    ax1 = fig.add_axes((0.1,0.1,0.7,0.7))
+    ax1.set_xlabel("Birth year")
+    ax1.set_ylabel("Death year")
+    ax2 = fig.add_axes((0.1,0.8,0.7,0.18), sharex=ax1, **kwargs)
+    ax3 = fig.add_axes((0.8,0.1,0.1,0.7), sharey=ax1, **kwargs)
+    ax4 = fig.add_axes((0.91,0.1,0.02,0.7))
+
+    plt.setp(ax2.get_xticklabels(), visible=False)
+    plt.setp(ax2.get_yticklabels(), visible=False)
+    plt.setp(ax3.get_xticklabels(), visible=False)
+    plt.setp(ax3.get_yticklabels(), visible=False)
+    ax2.bar(Yn, Nn, 1, color="#559")
+    ax3.barh(Yd, Nd, 1, color="#644")
+
+    cm = ax1.pcolor(X, Y, count, cmap="nipy_spectral")
+    ax1.set_xlim(1891, 2023)
+    ax1.set_ylim(1972, 2023)
+
+    fig.colorbar(cm, cax=ax4)
+    return fig
+############################
+## Setup plots parameters ##
+############################
+
 sns.set_theme(style="dark")
 plt.style.use('dark_background')
 plt.rcParams.update({
@@ -13,12 +75,19 @@ plt.rcParams.update({
     "axes.axisbelow" : True
 })
 
-ms_year = 1000 * 86400 * 365.25
+###############################
+## Data recovery and parsing ##
+###############################
 
+# Constents
+
+# Number of ms in one tropical year
+ms_year = 1000 * 86400 * 365.24219
+
+# Data
 df = pl.scan_parquet("all_décès.parquet")
 
-# remove all the uncanny dates
-
+# Remove all the uncanny dates
 sex = pl.col("Sexe")
 dd = pl.col("Date_décès")
 dn = pl.col("Date_naissance")
@@ -31,6 +100,7 @@ yd = dd.dt.year()
 uncanny = (yd <= 2023) & (yd >= 1970) & (dd >= dn) & (yn <= 2023) & (yn >= 1870)
 df = df.filter(uncanny)
 
+# Recover data
 data = df.select([
     sex,
     dn,
@@ -39,27 +109,14 @@ data = df.select([
     
 ]).collect()
 
+##################
+## Computations ##
+##################
+
 yd = data["Date_décès"].dt.year()
 yn = data["Date_naissance"].dt.year()
-Yd, Nd = np.unique(yd, return_counts=True)
-Yn, Nn = np.unique(yn, return_counts=True)
-
-
-fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-fig.set_size_inches(12, 4)
-
-ax1.bar(Yn, Nn, 1, color="#559")
-ax1.legend(["Birth years of the desceased"])
-ax1.grid(linestyle="--")
-ax1.set_ylabel("Number")
-ax1.set_xlabel("Year")
-
-ax2.bar(Yd, Nd, 1, color="#644")
-ax2.legend(["Years of the deaths"])
-ax2.grid(linestyle="--")
-ax2.set_xlabel("Year")
-fig.savefig("figures/year_dist.svg")
-plt.show()
+Death = np.unique(yd, return_counts=True)
+Born = np.unique(yn, return_counts=True)
 
 md = yd.min()
 mn = yn.min()
@@ -68,33 +125,16 @@ N = (yn - mn).to_numpy()
 rd, rn = D.max() + 1, N.max() + 1
 count = np.bincount(D + N*rd,  minlength=rn*rd)
 count = count.reshape(rn, rd).T
+X, Y = np.arange(mn, mn+rn+1) - 0.5, np.arange(md, md+rd+1) - 0.5
 
-kwargs = {
-        "frameon" : False,
-        "xmargin" : 0,
-        "ymargin" : 0,
-        }
+##################
+## Plot figures ##
+##################
 
-fig = plt.figure()
-fig.set_size_inches(8, 8)
-ax1 = fig.add_axes((0.1,0.1,0.7,0.7))
-ax1.set_xlabel("Birth year")
-ax1.set_ylabel("Death year")
-ax2 = fig.add_axes((0.1,0.8,0.7,0.18), sharex=ax1, **kwargs)
-ax3 = fig.add_axes((0.8,0.1,0.1,0.7), sharey=ax1, **kwargs)
-ax4 = fig.add_axes((0.91,0.1,0.02,0.7))
+fig = fig_histogram(Born, Death)
+fig.savefig("figures/year_dist.svg")
+plt.show()
 
-plt.setp(ax2.get_xticklabels(), visible=False)
-plt.setp(ax2.get_yticklabels(), visible=False)
-plt.setp(ax3.get_xticklabels(), visible=False)
-plt.setp(ax3.get_yticklabels(), visible=False)
-ax2.bar(Yn, Nn, 1, color="#559")
-ax3.barh(Yd, Nd, 1, color="#644")
-
-cm = ax1.pcolor(np.arange(mn, mn+rn+1) - 0.5, np.arange(md, md+rd+1) - 0.5, count, cmap="nipy_spectral")
-ax1.set_xlim(1891, 2023)
-ax1.set_ylim(1972, 2023)
-
-fig.colorbar(cm, cax=ax4)
+fig = fig_2Dhist(Born, Death, X, Y, count)
 fig.savefig("figures/year_dist_2D.svg")
 plt.show()
