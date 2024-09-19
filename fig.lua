a30328c2c788a36fb80efd544f61a8b8d7db40d8
Pacidus <pacidus@gmail.com>
2024-09-19 16:10:08 +0200

    aled

--- fig.lua
+++ fig.lua
@@ -1,9 +1,17 @@
 if FORMAT:match "gfm" then
-    function Figcaption (elem)
+    function Figcaptionqq (elem)
         return {
         pandoc.RawInline("html", "<p align='center'>"),
         elem,
         pandoc.RawInline("html", "</p>")
         }
     end
+
+    function Figure (elem)
+        elem.attributes.style = 'text-align: center; margin:auto; display: block'
+        return elem 
+    end
 end
+
+
+
